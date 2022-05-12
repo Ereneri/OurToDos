d10d9cf0e96c12f68f2447b4c09c2d3159e5e558
Eren Erisgen <erenerisgen@Erens-MBP.ad.edenpr.org>
2022-05-12 11:36:54 -0500

    added basic starterfiles

--- ourtodos/ourtodos/settings.py
+++ ourtodos/ourtodos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ourtodos',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -80,6 +82,8 @@ DATABASES = {
     }
 }
 
+#AUTH_USER_MODEL = 'ourtodos.User'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -116,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ourtodos/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ ourtodos/ourtodos/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django import forms
+from django.contrib import messages
+
+def index(request):
+    return render(request, "ourtodos/index.html")
