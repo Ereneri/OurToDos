016b261ab9c3e027b6a07dd53168296619be354d
Eren Erisgen <erenerisgen@Erens-MBP.ad.edenpr.org>
2022-05-13 10:58:25 -0500

    resetting django

--- ourtodos/ourtodos/views.py
+++ ourtodos/ourtodos/views.py
@@ -8,3 +8,19 @@ from django.contrib import messages
 
 def index(request):
     return render(request, "ourtodos/index.html")
+
+# creates a new user
+def register(request):
+    # Needs user's email, username, and password
+    # The password needs to be hashed and salted into SQL db
+    pass;
+
+# logins in user
+def login(request):
+    # calls username and hashes and salts and checks against SQL db
+    pass
+
+# Creates new task with user session data
+def createTodo():
+    pass
+
