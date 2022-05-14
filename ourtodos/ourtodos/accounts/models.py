426ecc1238b074a05861514a1580c3b8e5ba94be
Eren Erisgen <ederisgen@gmail.com>
2022-05-13 22:05:51 -0500

    added User Models

--- ourtodos/ourtodos/accounts/models.py
+++ ourtodos/ourtodos/accounts/models.py
@@ -7,13 +7,13 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 # Create your models here.
 class UserManager(BaseUserManager):
 
-    def create_user(self, name, password, email):
+    def create_user(self, name, password, email, **other_fields):
 
         if not email:
             raise ValueError(gettext_lazy('Users must provide an email address'))
 
         email = self.normalize_email(email)
-        user = self.model(name=name, email=email)
+        user = self.model(name=name, email=email, **other_fields)
         user.set_password(password)
         user.save()
 
@@ -29,7 +29,7 @@ class UserManager(BaseUserManager):
         if other_fields.get('is_superuser') is not True:
             raise ValueError(gettext_lazy('Superuser must have is_superuser=True'))
 
-        return self.create_user(name, password, email)
+        return self.create_user(name, password, email, **other_fields)
 
 class NewUser(AbstractBaseUser, PermissionsMixin):
 
@@ -38,6 +38,7 @@ class NewUser(AbstractBaseUser, PermissionsMixin):
     password = models.CharField(max_length=100)
     email = models.EmailField(gettext_lazy('email address'), unique=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     objects = UserManager()
 
