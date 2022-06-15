bb708f47d40409344717a42ed942e6a0343c94ac
Eren Erisgen <ederisgen@gmail.com>
2022-06-15 09:18:35 -0500

    loaded JS from static

--- ourtodos/ourtodos/accounts/static/accounts/todo.js
+++ ourtodos/ourtodos/accounts/static/accounts/todo.js
@@ -1,21 +1,43 @@
-function getCookie(name) {
-let cookieValue = null;
-if (document.cookie && document.cookie !== '') {
-    const cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim();
-        // Does this cookie string begin with the name we want?
-        if (cookie.substring(0, name.length + 1) === (name + '=')) {
-            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-            break;
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("dom loaded");
+
+    // basic buttons for complete and uncomplete
+    document.addEventListener('click', event => {
+        if (event.target.className === 'complete') {
+            const todoId = event.target.parentNode.id;
+            console.log("parentnode " + todoId);
+            const todo = document.getElementById(todoId);
+            console.log("getelementbyID " + todo);
+        }
+    });
+
+    function updateComplete(todoId) {
+        const todo = document.getElementById(todoId);
+        fetch('/task-edit/' + todoId, {
+            method: 'PUT',
+            body: JSON.stringify({ complete : !todo.complete }),
+          })
+          console.log("edit made");
+    }
+
+
+    function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
         }
     }
-}
-return cookieValue;
-}
-// get csrftoken 
-const csrftoken = getCookie('csrftoken');
+    return cookieValue;
+    }
+    // get csrftoken 
+    const csrftoken = getCookie('csrftoken');
 
-const edits = document.querySelectorAll('.edit');
-document.addEventListener('click', event => {
-    
+    const edits = document.querySelectorAll('.edit');
+});
